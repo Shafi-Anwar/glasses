76221f91897a008c5444612759cd0a4daf938e41
Shafi Anwar <dws@DWSs-MacBook-Pro.local>
2024-03-10 19:15:38 +0530

    first commit:

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css"
+function Hero() {
+    return (
+        <div className="hero-container">
+            <h1>Hero Section</h1>
+        </div>
+    );
+}
